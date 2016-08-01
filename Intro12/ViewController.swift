a924f50fed462164153b84819615656a3d46bc21
SwiftiCode <swifticode@gmail.com>
2016-08-01 11:49:41 +0800

    Final

--- Intro12/ViewController.swift
+++ Intro12/ViewController.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // MARK: Properties
+    @IBOutlet weak var myImageView: UIImageView!
 
+    
+    // MARK: Default Template
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +24,32 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        
+        myImageView.image = image
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 
+    // MARK: Action
+    @IBAction func choosePicture(sender: UITapGestureRecognizer) {
+        
+        let myImagePickerController = UIImagePickerController()
+        
+        myImagePickerController.sourceType = .PhotoLibrary
+        myImagePickerController.delegate = self
+        
+        presentViewController(myImagePickerController, animated: true, completion: nil)
+        
+    }
+    
 
 }
 
